9d1ccecab125e0abcb0b47a74251d56c4e7634c4
Alex Libertowski <Alex.Libertowski@outlook.com>
2020-01-15 12:25:57 -0500

    Added the Blackjack project

---
+++ Blackjack/Blackjack/Blackjack.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Blackjack
+{
+    partial class Blackjack
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstCard = new System.Windows.Forms.Label();
+            this.secondCard = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.hitButton = new System.Windows.Forms.Button();
+            this.dealerTotalLabel = new System.Windows.Forms.Label();
+            this.firstDealerCard = new System.Windows.Forms.Label();
+            this.secondDealerCard = new System.Windows.Forms.Label();
+            this.stayButton = new System.Windows.Forms.Button();
+            this.aceLabel = new System.Windows.Forms.Label();
+            this.deckLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.lossLabel = new System.Windows.Forms.Label();
+            this.WLRLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstCard
+            // 
+            this.firstCard.AutoSize = true;
+            this.firstCard.Location = new System.Drawing.Point(12, 127);
+            this.firstCard.Name = "firstCard";
+            this.firstCard.Size = new System.Drawing.Size(19, 13);
+            this.firstCard.TabIndex = 0;
+            this.firstCard.Text = "10";
+            // 
+            // secondCard
+            // 
+            this.secondCard.AutoSize = true;
+            this.secondCard.Location = new System.Drawing.Point(37, 127);
+            this.secondCard.Name = "secondCard";
+            this.secondCard.Size = new System.Drawing.Size(19, 13);
+            this.secondCard.TabIndex = 1;
+            this.secondCard.Text = "10";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(12, 103);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(49, 13);
+            this.totalLabel.TabIndex = 2;
+            this.totalLabel.Text = "Total: 10";
+            // 
+            // hitButton
+            // 
+            this.hitButton.Location = new System.Drawing.Point(15, 185);
+            this.hitButton.Name = "hitButton";
+            this.hitButton.Size = new System.Drawing.Size(75, 23);
+            this.hitButton.TabIndex = 3;
+            this.hitButton.Text = "Hit";
+            this.hitButton.UseVisualStyleBackColor = true;
+            this.hitButton.Click += new System.EventHandler(this.hitButton_Click);
+            // 
+            // dealerTotalLabel
+            // 
+            this.dealerTotalLabel.AutoSize = true;
+            this.dealerTotalLabel.Location = new System.Drawing.Point(12, 44);
+            this.dealerTotalLabel.Name = "dealerTotalLabel";
+            this.dealerTotalLabel.Size = new System.Drawing.Size(83, 13);
+            this.dealerTotalLabel.TabIndex = 4;
+            this.dealerTotalLabel.Text = "Dealer Total: 10";
+            // 
+            // firstDealerCard
+            // 
+            this.firstDealerCard.AutoSize = true;
+            this.firstDealerCard.Location = new System.Drawing.Point(12, 68);
+            this.firstDealerCard.Name = "firstDealerCard";
+            this.firstDealerCard.Size = new System.Drawing.Size(19, 13);
+            this.firstDealerCard.TabIndex = 5;
+            this.firstDealerCard.Text = "10";
+            this.firstDealerCard.Visible = false;
+            // 
+            // secondDealerCard
+            // 
+            this.secondDealerCard.AutoSize = true;
+            this.secondDealerCard.Location = new System.Drawing.Point(37, 68);
+            this.secondDealerCard.Name = "secondDealerCard";
+            this.secondDealerCard.Size = new System.Drawing.Size(19, 13);
+            this.secondDealerCard.TabIndex = 6;
+            this.secondDealerCard.Text = "10";
+            // 
+            // stayButton
+            // 
+            this.stayButton.Location = new System.Drawing.Point(126, 185);
+            this.stayButton.Name = "stayButton";
+            this.stayButton.Size = new System.Drawing.Size(75, 23);
+            this.stayButton.TabIndex = 7;
+            this.stayButton.Text = "Stay";
+            this.stayButton.UseVisualStyleBackColor = true;
+            this.stayButton.Click += new System.EventHandler(this.stayButton_Click);
+            // 
+            // aceLabel
+            // 
+            this.aceLabel.AutoSize = true;
+            this.aceLabel.Location = new System.Drawing.Point(12, 153);
+            this.aceLabel.Name = "aceLabel";
+            this.aceLabel.Size = new System.Drawing.Size(0, 13);
+            this.aceLabel.TabIndex = 8;
+            // 
+            // deckLabel
+            // 
+            this.deckLabel.AutoSize = true;
+            this.deckLabel.Location = new System.Drawing.Point(12, 11);
+            this.deckLabel.Name = "deckLabel";
+            this.deckLabel.Size = new System.Drawing.Size(92, 13);
+            this.deckLabel.TabIndex = 9;
+            this.deckLabel.Text = "Cards in Deck: 52";
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(135, 52);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(43, 13);
+            this.winLabel.TabIndex = 10;
+            this.winLabel.Text = "Wins: 0";
+            // 
+            // lossLabel
+            // 
+            this.lossLabel.AutoSize = true;
+            this.lossLabel.Location = new System.Drawing.Point(135, 77);
+            this.lossLabel.Name = "lossLabel";
+            this.lossLabel.Size = new System.Drawing.Size(52, 13);
+            this.lossLabel.TabIndex = 11;
+            this.lossLabel.Text = "Losses: 0";
+            // 
+            // WLRLabel
+            // 
+            this.WLRLabel.AutoSize = true;
+            this.WLRLabel.Location = new System.Drawing.Point(135, 103);
+            this.WLRLabel.Name = "WLRLabel";
+            this.WLRLabel.Size = new System.Drawing.Size(84, 13);
+            this.WLRLabel.TabIndex = 12;
+            this.WLRLabel.Text = "W/L Ratio: 0.00";
+            // 
+            // Blackjack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 228);
+            this.Controls.Add(this.WLRLabel);
+            this.Controls.Add(this.lossLabel);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.deckLabel);
+            this.Controls.Add(this.aceLabel);
+            this.Controls.Add(this.stayButton);
+            this.Controls.Add(this.secondDealerCard);
+            this.Controls.Add(this.firstDealerCard);
+            this.Controls.Add(this.dealerTotalLabel);
+            this.Controls.Add(this.hitButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.secondCard);
+            this.Controls.Add(this.firstCard);
+            this.Name = "Blackjack";
+            this.Text = "Blackjack";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstCard;
+        private System.Windows.Forms.Label secondCard;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button hitButton;
+        private System.Windows.Forms.Label dealerTotalLabel;
+        private System.Windows.Forms.Label firstDealerCard;
+        private System.Windows.Forms.Label secondDealerCard;
+        private System.Windows.Forms.Button stayButton;
+        private System.Windows.Forms.Label aceLabel;
+        private System.Windows.Forms.Label deckLabel;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label lossLabel;
+        private System.Windows.Forms.Label WLRLabel;
+    }
+}
+
---
+++ Blackjack/Blackjack/Blackjack.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Blackjack
+{
+    public partial class Blackjack : Form
+    {
+        const int MAX_CARDS = 52, BLACKJACK = 21;
+        readonly int[] staticDeck = new int[MAX_CARDS];
+        Random rng = new Random();
+
+        List<int> deck = new List<int>();
+        List<int> discardDeck = new List<int>();
+        List<int> cardsInHand = new List<int>();
+        List<int> dealerHand = new List<int>();
+
+        Point lastLabelPosition, lastDealerLabelPosition;
+        int playerTotal = 0, dealerTotal = 0, wins = 0, losses = 0;
+
+        public Blackjack()
+        {
+            InitializeComponent();
+
+            // Sets all the cards in the static deck
+            int cardNumber = 1;
+            int cardAmount = 1;
+            for (int i = 0; i < MAX_CARDS; i++)
+            {
+                staticDeck[i] = cardNumber;
+                discardDeck.Add(cardNumber);
+                if (++cardAmount == 5 && cardNumber != 10)
+                {
+                    ++cardNumber;
+                    cardAmount = 1;
+                }
+            }
+            Shuffle(); // Shuffles the dynamic deck
+
+            // Initializations
+            AddCard(firstCard, true);
+            AddCard(secondCard, true);
+            firstCard.Click += Label_Clicked;
+            secondCard.Click += Label_Clicked;
+
+            AddCard(firstDealerCard, false);
+            AddCard(secondDealerCard, false);
+            if (secondDealerCard.Text == "A")
+            {
+                dealerTotalLabel.Text = "Dealer Total: 11";
+            }
+            else
+            {
+                dealerTotalLabel.Text = "Dealer Total: " + secondDealerCard.Text;
+            }
+
+            firstDealerCard.Click += Label_Clicked;
+            secondDealerCard.Click += Label_Clicked;
+
+            lastLabelPosition = secondCard.Location;
+            lastDealerLabelPosition = secondDealerCard.Location;
+        }
+
+        private void Shuffle()
+        {
+            // Adds the all the discarded cards to a temp list
+            List<int> addedIndexes = new List<int>();
+            for (int i = 0; i < discardDeck.Count; i++)
+            {
+                addedIndexes.Add(discardDeck[i]);
+            }
+
+            // Randomly generates an index in the temp list and adds it to the deck to shuffle
+            while (addedIndexes.Count > 0)
+            {
+                int cardIndex = rng.Next(0, addedIndexes.Count);
+                deck.Add(addedIndexes[cardIndex]);
+                addedIndexes.RemoveAt(cardIndex);
+            }
+            discardDeck.Clear();
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+            Label nextCard = new Label();
+            nextCard.AutoSize = true;
+            nextCard.Name = "label";
+            nextCard.Click += Label_Clicked;
+            nextCard.Location = new Point(lastLabelPosition.X + 25, lastLabelPosition.Y);
+            lastLabelPosition = nextCard.Location;
+            AddCard(nextCard, true);
+
+            Controls.Add(nextCard);
+
+            if (AceCheckFailed())
+            {
+                firstDealerCard.Visible = true;
+                dealerTotalLabel.Text = "Dealer Total: " + dealerTotal.ToString();
+                MessageBox.Show("You Busted!", "Game Ended");
+                Restart(false);
+            }
+        }
+
+        private void AddCard(Label cardLabel, bool isPlayer)
+        {
+            // Checks if the deck has any cards and then shuffles       
+            if(deck.Count == 0)
+            {
+                Shuffle();
+            }
+
+            // Checks if the next card was an ace and changes the label to an A
+            if (deck[0] == 1)
+            {
+                cardLabel.Text = "A";
+            }
+            else
+            {
+                cardLabel.Text = deck[0].ToString();
+            }
+
+            if (isPlayer)
+            {
+                // Makes the aces an 11 by default
+                if (deck[0] == 1)
+                {
+                    cardsInHand.Add(11);
+                    cardLabel.Tag = 11;
+                }
+                else
+                {
+                    cardsInHand.Add(deck[0]);
+                }
+
+                playerTotal += cardsInHand[cardsInHand.Count - 1];
+                totalLabel.Text = "Total: " + playerTotal.ToString();
+
+
+            }
+            else        
+            {
+                // Makes the aces an 11 by default
+                if (deck[0] == 1)
+                {
+                    dealerHand.Add(11);
+                    cardLabel.Tag = 11;
+                }
+                else
+                {
+                    dealerHand.Add(deck[0]);
+                }
+
+                dealerTotal += dealerHand[dealerHand.Count - 1];
+                dealerTotalLabel.Text = "Dealer Total: " + dealerTotal.ToString();
+            }
+
+            // Removes the top card
+            discardDeck.Add(deck[0]);
+            deck.RemoveAt(0);
+            deckLabel.Text = "Cards in Deck: " + deck.Count;
+        }
+
+        private void Restart(bool didPlayerWin)
+        {
+            cardsInHand.Clear();
+            dealerHand.Clear();
+            firstDealerCard.Visible = false;
+            aceLabel.Text = string.Empty;
+            lastLabelPosition = secondCard.Location;
+            lastDealerLabelPosition = secondDealerCard.Location;
+            playerTotal = 0;
+            dealerTotal = 0;
+
+            List<Label> controlsToDelete = new List<Label>();
+            foreach (Control value in Controls)
+            {
+                if (value.Name == "label")
+                {
+                    Label extraLabel = (Label)value;
+                    controlsToDelete.Add(extraLabel);
+                }
+            }
+
+            foreach (Label value in controlsToDelete)
+            {
+                Controls.Remove(value);
+            }
+
+            AddCard(firstCard, true);
+            AddCard(secondCard, true);
+
+            AddCard(firstDealerCard, false);
+            AddCard(secondDealerCard, false);
+            if (secondDealerCard.Text == "A")
+            {
+                dealerTotalLabel.Text = "Dealer Total: 11";
+            }
+            else
+            {
+                dealerTotalLabel.Text = "Dealer Total: " + secondDealerCard.Text;
+            }
+
+            if (didPlayerWin)
+            {
+                ++wins;
+                winLabel.Text = "Wins: " + wins.ToString();
+            }
+            else
+            {
+                ++losses;
+                lossLabel.Text = "Losses: " + losses.ToString();
+            }
+
+            float wlRatio = (float)wins / losses;
+            WLRLabel.Text = "W/L Ratio: " + wlRatio.ToString("F2");
+        }
+
+        private void stayButton_Click(object sender, EventArgs e)
+        {
+            // The dealer keeps hitting until they're at or above 16
+            while (dealerTotal < 16)
+            {
+                Label nextCard = new Label();
+                nextCard.AutoSize = true;
+                nextCard.Name = "label";
+                nextCard.Click += Label_Clicked;
+                nextCard.Location = new Point(lastDealerLabelPosition.X + 25, lastDealerLabelPosition.Y);
+                lastDealerLabelPosition = nextCard.Location;
+
+                AddCard(nextCard, false);
+                Controls.Add(nextCard);
+            }
+            dealerTotalLabel.Text = "Dealer Total: " + dealerTotal.ToString();
+            firstDealerCard.Visible = true;
+
+            bool didPlayerWin = false;
+            if ((dealerTotal > BLACKJACK || playerTotal > dealerTotal) && playerTotal <= BLACKJACK)
+            {
+                MessageBox.Show("You Won!", "Game Ended");
+                didPlayerWin = true;
+            }
+            else if (playerTotal == dealerTotal)
+            {
+                if (cardsInHand.Count > dealerHand.Count)
+                {
+                    MessageBox.Show("You Lost!", "Game Ended");
+                }
+                else if(cardsInHand.Count < dealerHand.Count)
+                {
+                    MessageBox.Show("You Won!", "Game Ended");
+                    didPlayerWin = true;
+                }
+                else
+                {
+                    MessageBox.Show("You Tied!", "Game Ended");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You Lost!", "Game Ended");
+            }
+            Restart(didPlayerWin);
+        }
+
+        private void Label_Clicked(object sender, EventArgs e)
+        {
+            Label labelClicked = (Label)sender;
+            if (labelClicked.Text == "A")
+            {
+                for (int i = 0; i < cardsInHand.Count; i++)
+                {
+                    if (cardsInHand[i] == (int)labelClicked.Tag)
+                    {
+                        if (cardsInHand[i] == 1)
+                        {
+                            cardsInHand[i] = 11;
+                            playerTotal = --playerTotal + 11;
+                            labelClicked.Tag = 11;
+                            break;
+                        }
+                        else
+                        {
+                            cardsInHand[i] = 1;
+                            playerTotal = ++playerTotal - 11;
+                            labelClicked.Tag = 1;
+                            break;
+                        }
+                    }
+                }
+
+                totalLabel.Text = "Total: " + playerTotal.ToString();
+                if (playerTotal > BLACKJACK)
+                {
+                    aceLabel.Text = "Your ace can be switched";
+                }
+                else
+                {
+                    aceLabel.Text = string.Empty;
+                }
+            }
+        }
+
+        // Checks if the player bust or not depending on whether the ace values are switched
+        private bool AceCheckFailed()
+        {
+            int handLength = cardsInHand.Count;
+
+            int[] tempPlayerHand = new int[handLength];
+            List<int> aceIndexes = new List<int>();
+
+            for (int i = 0; i < handLength; ++i)
+            {
+                int cardValue = cardsInHand[i];
+                tempPlayerHand[i] = cardValue;
+
+                if (cardValue == 1 || cardValue == 11)
+                {
+                    aceIndexes.Add(i);
+                }
+            }
+
+            int firstIndex = -1;
+            foreach(int i in aceIndexes)
+            {
+                if (firstIndex == -1)
+                {
+                    tempPlayerHand[i] = 11;
+                    firstIndex = i;
+                }
+                else
+                {
+                    tempPlayerHand[i] = 1;
+                }
+            }
+
+            int tempTotal = tempPlayerHand.Sum();
+            if (firstIndex != -1)
+            {
+                if (tempTotal > BLACKJACK)
+                {
+                    tempPlayerHand[firstIndex] = 1;
+                    tempTotal = tempPlayerHand.Sum();
+
+                    if (tempTotal > BLACKJACK)
+                    {
+                        totalLabel.Text = "Total: " + tempTotal.ToString();
+                        return true;
+                    }
+                    else
+                    {
+                        aceLabel.Text = "Your ace can be switched";
+                        return false;
+                    }
+                }
+            }
+
+            if (playerTotal > BLACKJACK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
